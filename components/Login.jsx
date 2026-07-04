721d34050378baba869637dfcdface26ce866c50
Thomas van Bockel <Tnvanbockel@gmail.com>
2026-07-04 13:02:45 +0200

    added login

---
+++ components/Login.jsx
@@ -0,0 +1,30 @@
+import {useContext, useEffect} from "react";
+import {View, Text} from "react-native";
+import * as LocalAuthentication from "expo-local-authentication";
+import {ThemeContext} from "../providers/ThemeProvider";
+
+export function Login({setLoggedIn}) {
+    const {theme} = useContext(ThemeContext);
+    useEffect(() => {
+        const login = async () => {
+            const result = await LocalAuthentication.authenticateAsync({
+                promptMessage: "Log in",
+            });
+
+            if (result.success) {
+                setLoggedIn(true);
+            }
+        };
+
+        const timeout = setTimeout(login, 300);
+
+        return () => clearTimeout(timeout);
+    }, []);
+
+    return (
+        <View
+            className={theme ? "flex-1 justify-center items-center bg-gray-600" : "flex-1 justify-center items-center bg-white"}>
+            <Text className={theme ? "color-white" : "color-black"}>Inloggen</Text>
+        </View>
+    );
+}
